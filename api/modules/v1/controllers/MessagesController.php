8ddbad7e6f810ee84496c84e69740ba731502bbd
ZapriyRoman <zapriy.05@mail.ru>
2015-10-22 16:59:19 +0300

    Saving message in DB

--- api/modules/v1/controllers/MessagesController.php
+++ api/modules/v1/controllers/MessagesController.php
@@ -5,6 +5,8 @@ namespace api\modules\v1\controllers;
 use Yii;
 use yii\rest\ActiveController;
 use yii\data\ActiveDataProvider;
+use api\modules\v1\models\Messages;
+use api\modules\v1\models\Salamiuser;
 
 class MessagesController extends ActiveController
 {
@@ -40,17 +42,33 @@ class MessagesController extends ActiveController
 
     public function actionSend(){
         if (Yii::$app->request->post("recipient")) {
+            $recipient_id = Yii::$app->request->post("recipient");
+            $text = Yii::$app->request->post("message");
+            $sender_id = Yii::$app->request->post("sender");
+            $recipientUser = Salamiuser::find()->where(['facebook_id' => $recipient_id])->one();
+            $senderUser = Salamiuser::find()->where(['facebook_id' => $sender_id])->one();
+
+            $message = new Messages;
+            $message->sender_id = $senderUser->id;
+            $message->recipient_id = $recipientUser->id;
+            $message->text = $text;
+            $message->created_at = time();
+            $message->state = "new";
+            $saved = $message->save();
+
             $data = array(
-              'user_ids' => array(Yii::$app->request->post("recipient")),
+              'user_ids' => array($recipient_id),
               'notification' => array(
-                "alert" => Yii::$app->request->post("message"),
+                "alert" => $text,
                 "android" => array(
                   'payload' => array(
-                    "sender" => Yii::$app->request->post("sender")
+                    "sender" => $sender_id
                   )
                 )
               )
             );
+            $ionicAppId = "70f2a984";
+            $ionicApiSecret = "ec4839bfb9ea2daa2271b0149a0c18003a3672178f4e6485";
             $url = "https://push.ionic.io/api/v1/push";
             $content = json_encode($data);
             $ch = curl_init('https://push.ionic.io/api/v1/push');
@@ -59,15 +77,15 @@ class MessagesController extends ActiveController
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($ch, CURLOPT_HTTPHEADER, array(
                 'Content-Type: application/json',
-                'X-Ionic-Application-Id: 70f2a984',
-                'Content-Length: ' + strlen($content),
-                'Authorization: Basic '.base64_encode("ec4839bfb9ea2daa2271b0149a0c18003a3672178f4e6485")
+                'X-Ionic-Application-Id: '.$ionicAppId,
+                'Content-Length: '.strlen($content),
+                'Authorization: Basic '.base64_encode($ionicApiSecret)
                 )
             );
 
             $result = curl_exec($ch);
             return $result;
         }
-        return "{}";
+        return '{"error": "Invalid request parameters"}';
     }
 }
